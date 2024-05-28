c0b3dac0886b9e23e84b99aad881112dd6946563
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-05-28 16:42:18 +0500

    Add files via upload

---
+++ Question No. 7/main.js
@@ -0,0 +1,5 @@
+// Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+console.log(5 + 3);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(24 / 3);
